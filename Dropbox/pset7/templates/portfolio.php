bb920d59400bd23375fe8eae5f0d4b05854351f7
samvt <samvantran@gmail.com>
2014-04-10 00:22:51 -0400

    added name and total

--- Dropbox/pset7/templates/portfolio.php
+++ Dropbox/pset7/templates/portfolio.php
@@ -1,22 +1,11 @@
 
-<!-- display portfolio -->
-
-
 <!-- Internal styling-->
 
     <style>
 
-    .portfolio {
-        width: 500px;
-        margin: auto;
-        border: 3px solid black;
-        text-align: center;
-    }
-    
-    tr.heading {
-        font-weight: bold;
-        text-align: center;
-    }
+    .portfolio { width: 600px; margin: auto; border: 3px solid black; }
+    tr.heading { font-weight: bold; text-align: center; }
+    td { padding-left: 25px; padding-right: 25px; }
 
     </style> 
     
@@ -30,16 +19,20 @@
     <table >
         <tr class="heading">
             <td>Symbol</td>
+            <td>Name</td>
             <td>Shares</td>
             <td>Price</td>
+            <td>Total</td>
         </tr>      
              
         <?php foreach ($positions as $position): ?>
             
             <tr>
                 <td><?= $position["symbol"] ?></td>
+                <td><?= $position["name"] ?></td>
                 <td><?= $position["shares"] ?></td>
                 <td><?= $position["price"] ?></td>
+                <td><?= print($position["shares"] * $position["price"]) ?>
             </tr>
 
         <?php endforeach ?>
